ebe66e8d31ffd5a8634ba44a153a790a58ef7785
Thomas Haller <Thomas.Haller@lab10.coop>
2018-11-11 21:12:14 +0100

    removed POA Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,13 +7,5 @@ import { constants } from '../constants'
 export const Footer = ({ netId }) => {
   const footerClassName = netId in constants.NETWORKS && constants.NETWORKS[netId].TESTNET ? 'sokol' : ''
 
-  return (
-    <footer className={`footer ${footerClassName}`}>
-      <div className="container">
-        <Link to="/poa-dapps-voting" className={`footer-logo ${footerClassName}`} />
-        <p className="footer-rights">{moment().format('YYYY')} POA Network. All rights reserved.</p>
-        <Socials />
-      </div>
-    </footer>
-  )
+  return <footer />
 }
