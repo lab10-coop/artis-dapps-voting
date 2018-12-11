ff81f65c84fb8bcdc4cd7078c41b2624c5435316
Thomas Haller <Thomas.Haller@lab10.coop>
2018-12-11 12:30:26 +0100

    new Validator Ballot: removed Name and Country requirements.

--- src/components/Validator.jsx
+++ src/components/Validator.jsx
@@ -25,41 +25,6 @@ export class Validator extends React.Component {
       onChange: e => validatorStore.changeValidatorMetadata(e, 'address'),
       id: 'address'
     }
-    const AutocompleteItem = ({ formattedSuggestion }) => (
-      <div className="custom-container">
-        <strong>{formattedSuggestion.mainText}</strong> <small>{formattedSuggestion.secondaryText}</small>
-      </div>
-    )
-    return (
-      <div>
-        <div className="hidden">
-          <div className="left">
-            <div className="form-el">
-              <label htmlFor="full-name">Full Name</label>
-              <input
-                type="text"
-                id="full-name"
-                value={validatorStore.fullName}
-                onChange={e => validatorStore.changeValidatorMetadata(e, 'fullName')}
-              />
-              <p className="hint">Proposed validator's full name. Example: Jefferson L. Flowers.</p>
-            </div>
-          </div>
-          <div className="left">
-            <div className="form-el">
-              <label htmlFor="country">Country</label>
-              <input
-                type="text"
-                id="country"
-                value={validatorStore.country}
-                onChange={e => validatorStore.changeValidatorMetadata(e, 'country')}
-              />
-              <p className="hint">Country of (tax) residence. Example: Italy.</p>
-            </div>
-          </div>
-        </div>
-        <hr />
-      </div>
-    )
+    return <div />
   }
 }
